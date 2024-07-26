552292c3a2b4dfd95a04ed56c67ff7558fc7b89d
Daniel King <daniel@mosaicml.com>
2024-07-25 23:09:17 -0700

    try

--- llmfoundry/data/packing.py
+++ llmfoundry/data/packing.py
@@ -476,11 +476,14 @@ def profile_packing(
     )
     train_dataloader = train_dataspec.dataloader
 
-    log.debug("Getting big batch")
-    big_batch = next(iter(train_dataloader))
+    log.debug("Getting iter")
+    dl_iter = iter(train_dataloader)
 
     # Get a bunch of raw examples
     if dist.get_local_rank() == 0:
+        log.debug("Getting big batch")
+        big_batch = next(dl_iter)
+
         # Cut everything down to size
         log.debug("Trimming big batch")
         sizes, trimmed_examples = _trim_batch(big_batch)
