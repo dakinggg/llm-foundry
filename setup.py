7922b844b8c0e901730a17d7f9133ae076b95f23
Daniel King <daniel@mosaicml.com>
2025-03-14 19:59:23 -0700

    back to 1.13

--- setup.py
+++ setup.py
@@ -124,7 +124,7 @@ extra_deps['megablocks'] = [
 ]
 
 extra_deps['te'] = [
-    'transformer-engine[pytorch]>=2.1,<2.2',
+    'transformer-engine[pytorch]==1.13',
 ]
 
 extra_deps['databricks-serverless'] = {
